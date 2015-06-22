da2bb7c907ab735c3a8764750a144bdb1df73160
andrewszw <zacharyandrews93@gmail.com>
2015-06-21 21:11:23 -0400

    initial commit

---
+++ app/src/main/java/com/example/andrewszw/travellogger/LogActivity.java
@@ -0,0 +1,16 @@
+package com.example.andrewszw.travellogger;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LogActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        // return new LogFragment();
+        return new Fragment();
+    }
+}
